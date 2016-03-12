0ea950cd801546a308e982d84999d41a11c202c4
Daniel S. Pereira <daniel.pereira@platis.global>
2016-03-12 17:21:13 -0300

    Initial commit

---
+++ app/src/androidTest/java/com/dansp/wi_fiunlock/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.dansp.wi_fiunlock;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
